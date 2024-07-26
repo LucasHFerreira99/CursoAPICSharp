56316130cb9ff5901f2184fb7157d4a1e42a5157
LucasHFerreira99 <lucashf99@outlook.com.br>
2024-07-26 17:35:10 -0300

    Ajustes e Otimizações - Serialização JSON

--- APICatalogo/APICatalogo/Program.cs
+++ APICatalogo/APICatalogo/Program.cs
@@ -1,12 +1,18 @@
 using APICatalogo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options=>
+    options.JsonSerializerOptions
+    .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
