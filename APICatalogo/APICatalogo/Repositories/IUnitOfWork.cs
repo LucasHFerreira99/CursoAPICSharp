100a7897094da567499e335dfdabf9d3ea6fe4ce
LucasHFerreira99 <lucashf99@outlook.com.br>
2024-08-01 16:04:09 -0300

    Implementando o padrão Unit Of Work

---
+++ APICatalogo/APICatalogo/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.Repositories;
+
+public interface IUnitOfWork
+{
+    //IRepository<Produto> ProdutoRepository { get; }
+    //IRepository<Categoria> CategoriaRepository { get; }
+    IProdutoRepository ProdutoRepository { get; }
+    ICategoriaRepository CategoriaRepository { get; }
+    void Commit();
+}
