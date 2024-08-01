2f102487fa8a6b9d633d6cb367364de1dcb714df
LucasHFerreira99 <lucashf99@outlook.com.br>
2024-08-01 13:31:44 -0300

    Implementando o repositório especifico em produtos

---
+++ APICatalogo/APICatalogo/Repositories/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.Repositories
+{
+    public interface IProdutoRepository
+    {
+        IEnumerable<Produto> GetProdutos();
+        Produto Get(int id);
+        Produto Create(Produto produto);
+        Produto Update(Produto produto);
+        Produto Delete(int id);
+
+    }
+}
