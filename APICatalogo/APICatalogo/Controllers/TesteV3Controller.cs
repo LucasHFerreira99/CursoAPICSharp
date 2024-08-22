a6d7b1fa23c7098026c8ae0de1c256ff3158427b
LucasHFerreira99 <lucashf99@outlook.com.br>
2024-08-22 17:21:38 -0300

    Versionamento da API : atributo MapToVersion

---
+++ APICatalogo/APICatalogo/Controllers/TesteV3Controller.cs
@@ -0,0 +1,27 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [ApiController]
+    [Route("api/teste")]
+    [ApiVersion(3)]
+    [ApiVersion(4)]
+    public class TesteV3Controller : ControllerBase
+    {
+        [MapToApiVersion(3)]
+        [HttpGet]
+        public string GetVersion3()
+        {
+            return "TesteV3 - Get - Api Versão 3.0";
+        }
+
+        [MapToApiVersion(4)]
+        [HttpGet]
+        public string GetVersion4()
+        {
+            return "TesteV4 - Get - Api Versão 4.0";
+        }
+    }
+}
